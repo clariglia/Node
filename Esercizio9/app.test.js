084676f9236b0d8b5b82461a1e5feea630ad0b60
clariglia <consigliadamaro0@gmail.com>
2022-07-16 18:02:24 +0200

    feat: all exercises

---
+++ Esercizio9/app.test.js
@@ -0,0 +1,14 @@
+const createApp = require('./app.js')
+const app = createApp()
+const supertest = require('supertest')
+const request = supertest(app)
+
+
+
+test('GET /', async () =>{
+    const response = await request.get('/')
+        .expect(200)
+        .expect("Content-Type", "text/html")
+        
+        expect(response.text).toEqual("<html><body><h1>Welcome to the World Wide Web!</h1></body></html>")
+});
